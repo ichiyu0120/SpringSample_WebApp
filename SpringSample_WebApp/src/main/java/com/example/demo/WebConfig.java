df53773b475b40e5a5153d391b5537db60b4c642
yichi <yichi@DESKTOP-GO8GQNL>
2019-09-04 10:15:04 +0900

    sample6-3-3 個別エラーメッセージ

---
+++ SpringSample_WebApp/src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public MessageSource messageSource() {
+		
+		ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+		
+		bean.setBasename("classpath:messages");
+		bean.setDefaultEncoding("UTF-8");
+		
+		return bean;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean localValidatorFactoryBean() {
+		
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.setValidationMessageSource(messageSource());
+		return localValidatorFactoryBean;
+	}
+}
--- SpringSample_WebApp/src/main/java/com/example/demo/model/SignupForm.java
+++ SpringSample_WebApp/src/main/java/com/example/demo/model/SignupForm.java
@@ -18,27 +18,27 @@ import lombok.Data;
 @Data
 public class SignupForm {
 	
-	@NotBlank
-	@Email
+	@NotBlank(message="{require_check}")
+	@Email(message="{email_check}")
 	private String userId;
 	
-	@NotBlank
-	@Length(min = 4,max = 100 )
-	@Pattern(regexp = "^[a-zA-Z0-9]+$")
+	@NotBlank(message="{require_check}")
+	@Length(min = 4,max = 100,message="{length_check}")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$",message="{pattern_check}")
 	private String password;
 	
-	@NotBlank
+	@NotBlank(message="{require_check}")
 	private String userName;
 	
-	@NotNull
+	@NotNull(message="{require_check}")
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	private Date birthday;
 	
-	@Min(20)
-	@Max(100)
+	@Min(value=20,message="{min_check}")
+	@Max(value=100,message="{max_check}")
 	private int age;
 	
-	@AssertFalse
+	@AssertFalse(message="{false_check}")
 	private boolean marriage;
 
 }
